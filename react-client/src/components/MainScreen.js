d3aec15bc2b501467a87b54e3aa2a573e1ffdb69
samer salmeh <salmeh.samer@gmail.com>
2018-09-16 15:53:15 +0300

    (FEAT) add Deleting Prompt Password

--- react-client/src/components/MainScreen.js
+++ react-client/src/components/MainScreen.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { Component } from "react";
 import { DragDropContainer, DropTarget } from "react-drag-drop-container";
-import SearchInput, {createFilter} from 'react-search-input'
+import SearchInput, { createFilter } from 'react-search-input'
 
 import { download, deleteFile } from "../../../renderer";
 import style from "./MainScreenStyle.css";
@@ -13,22 +13,27 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import Button from "@material-ui/core/Button"; 
+import Button from "@material-ui/core/Button";
 
 var shell = require("shelljs");
 require("shelljs-plugin-open");
 var ds = require("fd-diskspace");
+var sudo = require('sudo-prompt');
 
+var options = {
+  name: 'Electron',
+  icns: '../image/thakiLogo.png', // (optional)
+};
 var freeSpace;
 // Async
-ds.diskSpace(function(err, res) {
+ds.diskSpace(function (err, res) {
   if (err) throw err;
   freeSpace = res.total.free;
   console.log(res.total.free);
 });
 
 //you can add key of what you search for like : by name, by path
-const KEYS_TO_FILTERS = ['name']
+const KEYS_TO_FILTERS = ['name', 'path', 'kind']
 
 export default class MainScreen extends Component {
   constructor(props) {
@@ -38,6 +43,7 @@ export default class MainScreen extends Component {
       open: false,
       newdata: [],
       searchTerm: '',
+      filter: '',
       rights: false,
       src: "../src/image/trashCan.png",
       spaceUsed: false,
@@ -80,11 +86,17 @@ export default class MainScreen extends Component {
       src: "../src/image/trashCan.png",
     });
   }
-
+ 
   //this function send the path of file to delete function in render.js to remove it
   delete() {
-    deleteFile(this.state.pathStore);
-    this.handleClose();
+    var file = this.state.pathStore
+    sudo.exec('echo hello', options,
+      function (error, stdout, stderr) {
+        if (error) console.log("delete have been failed becuse ",error);
+    deleteFile(file);
+      }
+    );
+     this.handleClose();
   }
 
   //this function hold two api ... one to get data from server and one to get data from JSON file
@@ -119,22 +131,22 @@ export default class MainScreen extends Component {
 
   // function to open announcement Dialog about file rights
   rightDialog = (key) => {
-    this.setState({ rights: true,pathStore:key });
+    this.setState({ rights: true, pathStore: key });
   };
 
   //function to close all Dialogs
   handleClose = () => {
-    this.setState({ open: false, spaceUsed: false ,rights:false});
+    this.setState({ open: false, spaceUsed: false, rights: false });
   };
 
-  searchUpdated (term) {
+  searchUpdated(term) {
     console.log(term)
-    this.setState({searchTerm: term})
+    this.setState({ searchTerm: term })
   }
 
   // function to reqest dawnload path from server
   sendData() {
-    console.log("connecting to the server .....",this.state.pathStore);
+    console.log("connecting to the server .....", this.state.pathStore);
     axios
       .post("https://afternoon-anchorage-52422.herokuapp.com/api/v1/get/object", {
         fileName: this.state.pathStore,
@@ -161,79 +173,79 @@ export default class MainScreen extends Component {
   render() {
     // search process
     const search = this.state.newdata.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
-    
+
+
     // return files of server to the user screen as buttons
     const serverContent = this.state.serverData.length
       ? this.state.serverData.map((ele, i) => {
-          //get the raw name from server
-          var data = ele.Key.replace(/^.*[\\\/]/, "").slice(0, -4);
+        //get the raw name from server
+        var data = ele.Key.replace(/^.*[\\\/]/, "").slice(0, -4);
 
-          //check if the file exist or not
-          var found = false;
+        //check if the file exist or not
+        var found = false;
 
-          this.state.newdata.map((dataEle, index) => {
-            if (data === dataEle.name) {
-              found = true;
-            }
-          });
-
-          if (found === false) {
-            return (
-              <button
-                className={style.new}
-                key={i}
-                onClick={() => this.rightDialog(ele.Key)}
-              >
-                {data}
-              </button>
-            );
+        this.state.newdata.map((dataEle, index) => {
+          if (data === dataEle.name) {
+            found = true;
           }
-        })
-      : "fetching data ...";
-     
-      // return files of JSON file to the user screen as images
-    const installedApp = this.state.newdata.length
-      ?  search.map(file => {
-        return (
-            <DragDropContainer
-              onDrop={() => this.dropped(file.path)}
-              targetKey="delete"
-              key={file.name}
+        });
+
+        if (found === false) {
+          return (
+            <button
+              className={style.new}
+              key={i}
+              onClick={() => this.rightDialog(ele.Key)}
             >
-              <Grid className={style.app} item xs={2}>
-                <img
-                  onClick={() => this.open(file.path)}
-                  className={style.icon}
-                  src={file.image}
-                />
-                <br />
-                {file.name}
-              </Grid>
-            </DragDropContainer>
+              {data}
+            </button>
+          );
+        }
+      })
+      : "fetching data ...";
+
+    // return files of JSON file to the user screen as images
+    const installedApp = this.state.newdata.length
+      ? search.map(file => {
+        return (
+          <DragDropContainer
+            onDrop={() => this.dropped(file.path)}
+            targetKey="delete"
+            key={file.name}
+          >
+            <Grid className={style.app} item xs={2}>
+              <img
+                onClick={() => this.open(file.path)}
+                className={style.icon}
+                src={file.image}
+              />
+              <br />
+              {file.name}
+            </Grid>
+          </DragDropContainer>
         )
       })
       : "empty";
-      
+
     return (
       <div className={style.main}>
         <div className={style.serverContent}>{serverContent}</div>
-        
+
         <div className={style.head}>
           <Grid className={style.header} container spacing={24}>
             <Grid item xs={4}>
-             <div className={style.search}>
-      <SearchInput className={style.SearchInput} onChange={this.searchUpdated} />
-      <a href="#">
-      <div className={style.SearchIcon} />
-      </a>
-      </div>
-    
+              <div className={style.search}>
+                <SearchInput className={style.SearchInput} onChange={this.searchUpdated} />
+                <a href="#">
+                  <div className={style.SearchIcon} />
+                </a>
+              </div>
             </Grid>
             <Grid item xs={4} />
             <Grid item xs={4}>
-              {/* <button className={style.filter}>
-    <div className={style.FilterIcon}></div>
-  </button> */}
+              <button className={style.filter} onClick={() => this.searchUpdated("image")}>
+                <div className={style.FilterIcon}></div>
+              </button>
             </Grid>
           </Grid>
         </div>
@@ -247,19 +259,19 @@ export default class MainScreen extends Component {
         <div className={style.footer}>
           <Grid className={style.foot} container spacing={24}>
             <Grid item xs={3}>
-              {/* <a href="">
-  <img src="../src/image/home.png" />
-</a>*/}
+              <a onClick={() => this.searchUpdated("")}>
+                <img src="../src/image/home.png" />
+              </a>
             </Grid>
             <Grid item xs={3}>
-              {/*<a href="">
-<img src="../src/image/fav.png" />
-</a>*/}
+              {/* <a href="">
+                <img src="../src/image/fav.png" />
+              </a> */}
             </Grid>
             <Grid item xs={3}>
               {/* <a href="#" onClick={this.refresh.bind(this)}>
-                                <img src="../src/image/update.png" />
-                            </a>*/}
+                <img src="../src/image/update.png" />
+              </a> */}
             </Grid>
             <Grid item xs={3}>
               <DropTarget
@@ -327,42 +339,42 @@ export default class MainScreen extends Component {
           </DialogTitle>
           <DialogContent>
             <DialogContentText>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac
-              facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum
-              at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus
-              sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum
-              nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur
-              et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras
-              mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-              egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-              lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-              sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-              Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-              consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-              egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-              lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-              sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-              Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-              consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-              egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-              lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-              sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-              Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-              consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-              egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-              lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-              sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-              Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-              consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-              egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-              lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-              sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-              Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac
+                facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum
+                at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus
+                sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum
+                nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur
+                et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras
+                mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
+                egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
+                sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+                Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+                consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
+                egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
+                sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+                Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+                consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
+                egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
+                sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+                Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+                consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
+                egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
+                sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+                Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+                consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
+                egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
+                sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+                Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
             </DialogContentText>
           </DialogContent>
           <DialogActions>
